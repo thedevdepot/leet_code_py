590eaf5ccebe047fd58b964103b6ac84825d86e2
dev <tonyringertx@gmail.com>
2025-06-22 13:47:04 -0500

    init commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# I am learning DSA using python.
+## Here goes nothin...
+
+# first up two sum two_sum.py
+
---
+++ two_sum.py
@@ -0,0 +1,9 @@
+# Given an array of integers, return indices of the two numbers such that they add up to specific target.
+# You may assume that each input would be exactly one solution, and you may not use the same element twice.
+# 
+# Given nums [2,7,11,15], target = 9
+# Because num(0) + num(1) = 2 + 7 = 9
+# return = [0, 1] 
+
+class Solution:
+
