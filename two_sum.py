583e5b3b17b763b46fe7ac6e1bd6f54cde48a1bb
dev <tonyringertx@gmail.com>
2025-06-22 14:22:30 -0500

    twosum completed and README updated

--- two_sum.py
+++ two_sum.py
@@ -5,5 +5,29 @@
 # Because num(0) + num(1) = 2 + 7 = 9
 # return = [0, 1] 
 
-class Solution:
+from typing import List
 
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        prevMap = {}  # val : index
+
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            prevMap[n] = i
+        return []
+
+def main():
+    solution = Solution()
+    nums = [2, 7, 11, 15]
+    target = 9
+    result = solution.twoSum(nums, target)
+    print(result)
+
+if __name__ == "__main__":
+    main()
+
+
+# time o(n)
+# meme 0(n)
