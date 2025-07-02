b7948dc063922f7d63b56397005940f6c53bb065
dev <tonyringertx@gmail.com>
2025-07-01 19:08:52 -0500

    working on max sub array

---
+++ max_sub_array.py
@@ -0,0 +1,18 @@
+'''
+Leet code problem 53 maximum sub array
+Given an integer array nums, find the contiguous subarray (containing at least 1 number)
+which has the largest sum and returns its sum
+'''
+
+class Solution:
+    def maxSubArray(self: nums, List[int]) -> int:
+        maxSub = nums[i]
+
+
+
+
+
+
+if _name_ = "_main_":
+    main()
+
